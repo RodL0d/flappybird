5dbab8941af317beb875e41b12c1cf51a500b932
Kawadentinho <kawakru@gmail.com>
2024-08-19 09:38:43 -0300

    obstacu

--- My project/Assets/Scripts/Obstacles.cs
+++ My project/Assets/Scripts/Obstacles.cs
@@ -7,21 +7,34 @@ public class Obstacles : MonoBehaviour
 
     float speed = 3.5f;
     Rigidbody2D rigidbody2D;
-    Obstacles obstacles;
-    public bool visivel;
-    Renderer rend;
+    public GameObject obstacles;
+    Vector2 direction, screenBounds, spriteBounds;
+    SpriteRenderer spriteRender;
 
 
     void Start()
     {
-     
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRender = GetComponent<SpriteRenderer>();
+
+        rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        spriteBounds = spriteRender.sprite.bounds.size / 2;
+
     }
 
 
     void Update()
     {
-        if (obstacles.transform.position.x = )
+        if (transform.position.x >= screenBounds.x)
         {
+            Destroy(obstacles);
+        }
+        else
+        {
+            rigidbody2D.velocity = direction * speed;
 
         }
     }
