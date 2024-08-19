bd9cd630c6986946bf9fe87490a7d41e93cf8498
Kawadentinho <kawakru@gmail.com>
2024-08-19 09:17:30 -0300

    Obstáculo

---
+++ My project/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+
+    float speed = 3.5f;
+    Rigidbody2D rigidbody2D;
+    Obstacles obstacles;
+    public bool visivel;
+    Renderer rend;
+
+
+    void Start()
+    {
+     
+    }
+
+
+    void Update()
+    {
+        if (obstacles.transform.position.x = )
+        {
+
+        }
+    }
+}
